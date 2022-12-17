1f2250bd73d5a70ef59a0e72f7ff9242c83eba6e
VMahluza0 <mahluzavictor@gmail.com>
2022-12-17 16:48:37 +0200

    models updated with def __str__(self)->:return anything_str

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -74,7 +74,8 @@ class Lesson(models.Model):
     order = models.IntegerField(default=0)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     content = models.TextField()
-
+    def __str__(self):
+        return "Name: " + self.name + "," + "Description: " + self.description
 
 # Enrollment model
 # <HINT> Once a user enrolled a class, an enrollment entry should be created between the user and course
@@ -94,6 +95,9 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
+    def __str__(self) -> str:
+        return f"{self.user} enrolled in {self.course} in {self.date_enrolled} with the rating of {self.rating}"
+
 
 # <HINT> Create a Question Model with:
     # Used to persist question content for a course
@@ -112,12 +116,16 @@ class Question(models.Model):
 
     # <HINT> A sample model method to calculate if learner get the score of the question
     def is_get_score(self, selected_ids):
-       all_answers = self.choice_set.filter(is_correct=True).count()
-       selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-       if all_answers == selected_correct:
-           return True
-       else:
-           return False
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
+
+        def __str__(self) -> str:
+            return self.question_text
+        
 
 
 #  <HINT> Create a Choice Model with:
@@ -131,12 +139,21 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
+    def __str__(self) -> str:
+        return self.choice_text
+
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
 class Submission(models.Model):
-   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-   choices = models.ManyToManyField(Choice)
-   #Other fields and methods you would like to design
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
+    #Other fields and methods you would like to design
+
+    def __str__(self) -> str:
+        return f"{self.question} : {self.choices}"
+
+
+   
